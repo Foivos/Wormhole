cf3837909937032e5cc08f1deaa75fe166da5ef6
Foivos <foivos@umich.edu>
2013-03-20 23:36:06 -0400

    fixed a small bug

--- common/com/foivos/wormhole/ClientProxy.java
+++ common/com/foivos/wormhole/ClientProxy.java
@@ -1,20 +1,13 @@
 package com.foivos.wormhole;
 
-import net.minecraftforge.client.MinecraftForgeClient;
-
-import com.foivos.wormhole.transport.WormholeTubeRenderer;
-
-import cpw.mods.fml.client.registry.RenderingRegistry;
 
 /**
  * The client side proxy,, that handles renderring
  */
 public class ClientProxy extends CommonProxy {
 	
-	WormholeTubeRenderer wormholeTubeRendered = new WormholeTubeRenderer();
 	
 	@Override
     public void registerRenderers() {
-            RenderingRegistry.registerBlockHandler(wormholeTubeRendered);
     }
 }
